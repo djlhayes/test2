25e1989d41cfedd1431499b440fbc1333259f58e
djlhayes <dexter.hayes@manchester.ac.uk>
2023-02-21 14:54:33 +0000

    Update study_definition.py

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,12 +7,12 @@ from cohortextractor import (
     # NOQA
 )
 
-
+#think about study population for project
 
 start_date = "2019-01-01"
-#end_date = "2020-02-01"
+#end_date = "2020-01-01"
 end_date = datetime.today().strftime('%Y-%m-%d')
-index_year = 2019
+#index_year = 2019
 min_age = 14
 max_age = 55
 
@@ -20,7 +20,7 @@ max_age = 55
  
 study = StudyDefinition(
     default_expectations={
-        "date": {"earliest": "1900-01-01", "latest": "today"},
+        "date": {"earliest": "start_date", "latest": "end_date"},
         "rate": "uniform",
         "incidence": 0.5,
     },
@@ -29,36 +29,43 @@ study = StudyDefinition(
     index_date=start_date,
 
     #population=patients.registered_with_one_practice_between(
-    #   "2019-02-01", "2020-02-01"
+    #   "2019-01-01", "2021-01-01"
     #),
   
     population=patients.satisfying(
         """
-        NOT has_died
+        NOT 
+        has_died
         AND
         registered
         AND
         (age>=14)
-        AND
-        has_follow_up_previous_year
+        #AND
+        #has_follow_up_previous_year
         AND
         (sex="F")
         """,
 
-     has_died=patients.died_from_any_cause(
+        has_died=patients.died_from_any_cause(
             on_or_before="index_date",
             returning="binary_flag",
         ),
 
-    age=patients.age_as_of(
-        "2019-09-01",
-        return_expectations={
+        registered=patients.satisfying(
+            "registered_at_start",
+            registered_at_start=patients.registered_as_of("index_date"),
+        ),
+
+        age=patients.age_as_of(
+        "2019-01-01",
+         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
-        },
+            },
+        ),
     ),
 
-    age_cat=patients.categorised_as(
+        age_cat=patients.categorised_as(
         {
             "0":"DEFAULT",
             "0-4": """ age >= 0 AND age < 5""",
@@ -85,18 +92,18 @@ study = StudyDefinition(
                     "55-64": 0.11,
                     "65-74": 0.11,
                     "75+": 0.11,
-                }
+                    }
+                },
             },
-        },
-    ),
+        ),
 
 
-    bmi=patients.most_recent_bmi(
-        between=["2019-02-01", "2020-02-01"],
-        minimum_age_at_measurement=18,
-        include_measurement_date=True,
-        date_format="YYYY-MM",
-        return_expectations={
+        bmi=patients.most_recent_bmi(
+            between=["2019-02-01", "2020-02-01"],
+            minimum_age_at_measurement=18,
+            include_measurement_date=True,
+            date_format="YYYY-MM",
+            return_expectations={
             "date": {   
                 "earliest": "2019-02-01", 
                 "latest": "2020-02-01"},
@@ -108,29 +115,29 @@ study = StudyDefinition(
             },
         ),
 
-    sex=patients.sex(
-    return_expectations={
-        "rate": "universal",
-        "category": {"ratios": {"M": 0.49, "F": 0.51}},
-         },
-    ),
+        sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+            },
+        ),
 
-    ethnicity=patients.with_ethnicity_from_sus(
-    returning="group_6",
-    use_most_frequent_code=True,
-    return_expectations={
+        ethnicity=patients.with_ethnicity_from_sus(
+        returning="group_6",
+        use_most_frequent_code=True,
+        return_expectations={
             "category": {"ratios": {"1": 0.8, "2": 0.1, "3": 0.1}},
             "incidence": 0.75,
             },
-    ),
+        ),
 
-    region=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="nuts1_region_name",
-        return_expectations={
-            "rate": "universal",
-            "category": {
-                "ratios": {
+        region=patients.registered_practice_as_of(
+            "2020-02-01",
+            returning="nuts1_region_name",
+            return_expectations={
+                "rate": "universal",
+                "category": {
+                    "ratios": {
                     "North East": 0.1,
                     "North West": 0.1,
                     "Yorkshire and the Humber": 0.1,
@@ -139,22 +146,22 @@ study = StudyDefinition(
                     "East of England": 0.1,
                     "London": 0.2,
                     "South East": 0.2,
+                    },
                 },
             },
-        },
-    ),
+        ),
 
-    practice=patients.registered_practice_as_of(
-        "2020-02-01",
-        returning="pseudo_id",
-        return_expectations={"int": {"distribution": "normal",
+        practice=patients.registered_practice_as_of(
+            "2020-01-01",
+            returning="pseudo_id",
+            return_expectations={"int": {"distribution": "normal",
                                      "mean": 25, "stddev": 5}, "incidence": 1}
-    ),
+        ),
 
-    admitted=patients.admitted_to_hospital(
-        returning="binary_flag",
-        between=["2019-02-01", "2020-02-01"],
-        return_expectations={"incidence": 0.1},
+         admitted=patients.admitted_to_hospital(
+         returning="binary_flag",
+            between=["2019-02-01", "2020-02-01"],
+             return_expectations={"incidence": 0.1},
     ),
     
     #stp=patients.registered_practice_as_of(
